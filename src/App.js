d1deb49ca4087cff50acee37b1ad06ac0f98ab56
Pranjal Singh <singhpranjal6973@gmail.com>
2022-05-23 09:39:31 +0530

    done basic setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import React from "react";
+import Features from "./components/Features";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
+import Services from "./components/Services";
+import Recommend from "./components/Recommend";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <Hero />
+      <Services />
+      <Recommend />
+      <Features />
+      <Footer />
+    </>
+  );
 }
