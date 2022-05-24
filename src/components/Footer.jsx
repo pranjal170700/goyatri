117e237a92c62e188e6b710159f9c3cdacc51203
Pranjal Singh <singhpranjal6973@gmail.com>
2022-05-25 01:06:17 +0530

    make footer section in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,66 @@
 import React from "react";
-
+import styled from "styled-components";
+import { BsLinkedin, BsFacebook } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <FooterContainer>
+      <span>Copyright &copy; 2021 GoYtri. All rights reserved</span>
+      <ul className="links">
+        <li>
+          <a href="#hero">Home</a>
+        </li>
+        <li>
+          <a href="#services">About</a>
+        </li>
+        <li>
+          <a href="#recommend">Places</a>
+        </li>
+        <li>
+          <a href="#features">Features</a>
+        </li>
+      </ul>
+      <ul className="social__links">
+        <li>
+          <BsFacebook />
+        </li>
+        <li>
+          <AiFillInstagram />
+        </li>
+        <li>
+          <BsLinkedin />
+        </li>
+      </ul>
+    </FooterContainer>
+  );
 }
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #d0d8ff;
+  border-radius: 0.5rem;
+  padding: 2.5rem;
+  ul {
+    display: flex;
+    list-style-type: none;
+    gap: 2rem;
+    li {
+      a {
+        text-decoration: none;
+        color: black;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: #302ce9;
+        }
+      }
+      svg {
+        font-size: 1.3rem;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: #302ce9;
+        }
+      }
+    }
+  }
+`;
